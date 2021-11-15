aa53fcd9a9c88300f14900497bf714ba5b51f314
tweinert <tait.weinert@outlook.com>
2021-11-16 06:37:40 +1030

    basic math functions

---
+++ script.js
@@ -0,0 +1,20 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+console.log("add: " + add(6, 4));
+console.log("subtract: " + subtract(16, 6));
+console.log("multiply: " + multiply(5, 8));
+console.log("divide: " + divide(64, 8));
