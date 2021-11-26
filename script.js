95cd3f90b23356eeab9d16d3d911ba865f265bd6
tweinert <tait.weinert@outlook.com>
2021-11-26 12:33:09 +1030

    fixed repeat operator button presses

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const displayHistory = document.querySelector('#history');
 
 let input1 = null;
 let input2 = null;
+let inputNum = null;
 let selectedOperator = '';
 let hasOperator = false;
 
@@ -50,67 +51,93 @@ function operate(operator, a, b) {
 
 // handle button click
 function buttonClick(event) {
-    let inputNum = this.textContent;
+    inputNum = this.textContent;
     
     // if a number button is clicked
     if (this.id == "numberButton") {
-        displayInput.textContent += inputNum;
-        // set input1 or 2 depending on if operator has been selected
-        if (!hasOperator) {
-            if (input1 === null) {
-                input1 = inputNum;
-            } else {
-                input1 += inputNum;
-            }
-        } else if (input2 === null) {
-            input2 = inputNum;
-        } else {
-            input2 += inputNum;
-        }
+        handleNumberButtonClick();
     } else if (this.id == "operatorButton") {
-        if (!hasOperator) {
-            if (input1 != null) {
-                selectedOperator = inputNum;
-                hasOperator = true;
-                displayHistory.textContent = input1 + " " + selectedOperator;
-                displayInput.textContent = '';
-            }
-        } else {
-            // operate (previous operater)
-            console.log(selectedOperator);
-            let calcResult = operate(selectedOperator, Number(input1), Number(input2));
-            clearAll();
-            input1 = calcResult;
-            selectedOperator = inputNum;
-            hasOperator = true;
-            displayHistory.textContent = calcResult + " " + inputNum;
-            calcResult = null;
-        }
+        handleOperatorButtonClick();
     } else if (this.id == "equalsButton") {
-        console.log("equals clicked");
-        console.log(input1 + " : " + input2);
-        if (hasOperator && input1 != null && input2 != null) {
-            let calcResult = operate(selectedOperator, Number(input1), Number(input2));
-            displayHistory.textContent = input1 + selectedOperator + input2 + " = ";
-            clearAll();
-            selectedOperator = '';
-            input1 = calcResult;
-            displayInput.textContent += calcResult;
-            calcResult = null;
-        }
+        handleEqualsButtonClick();
     } else if (this.id == "clearButton") {
         clearAll();
         displayHistory.textContent = '';
     } else if (this.id == "backspaceButton") {
 
     } else if (this.id == "periodButton") {
-        
+
     }
 }
 
+function handleNumberButtonClick() {
+    displayInput.textContent += inputNum;
+    // set input1 or 2 depending on if operator has been selected
+    if (!hasOperator) {
+        if (input1 === null) {
+            input1 = inputNum;
+        } else {
+            input1 += inputNum;
+        }
+    } else if (input2 === null) {
+        input2 = inputNum;
+    } else {
+        input2 += inputNum;
+    }
+}
+
+function handleOperatorButtonClick() {
+    if (!hasOperator && input1 != null) {
+        // if first number selected but no operator
+        selectedOperator = inputNum;
+        hasOperator = true;
+        displayHistory.textContent = input1 + " " + selectedOperator;
+        displayInput.textContent = '';
+    } else if (hasOperator && input1 != null && input2 != null) {
+        // if both numbers and operator selected
+        // operate using previous operater
+        console.log(selectedOperator);
+        let calcResult = operate(selectedOperator, Number(input1), Number(input2));
+        clearAll();
+        input1 = calcResult;
+        selectedOperator = inputNum;
+        hasOperator = true;
+        displayHistory.textContent = calcResult + " " + inputNum;
+        calcResult = null;
+    } else if (hasOperator && input1 != null && input2 == null) {
+        // if first number and operator are selected but not second number
+        //     should only occur when pressing operator repeatedly
+        selectedOperator = inputNum;
+        displayHistory.textContent = input1 + " " + selectedOperator;
+        displayInput.textContent = '';
+    }
+}
+
+function handleEqualsButtonClick() {
+    if (hasOperator && input1 != null && input2 != null) {
+        let calcResult = operate(selectedOperator, Number(input1), Number(input2));
+        displayHistory.textContent = input1 + selectedOperator + input2 + " = ";
+        clearAll();
+        selectedOperator = '';
+        input1 = calcResult;
+        displayInput.textContent += calcResult;
+        calcResult = null;
+    }
+}
+
+
+
 function clearAll() {
-    input1 = '';
-    input2 = '';
+    input1 = null;
+    input2 = null;
     hasOperator = false;
     displayInput.textContent = '';
 }
+
+/*
+TODO
+operator pressed twice
+equals clicked without 2 inputs and operator
+backspace
+dot button
+*/
